ee3d392ab01501fd1dfbe4be8a469466d0cb3f7d
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-07 07:52:11 +0530

    Reverse Number

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+namespace ReverseNumber
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            int n, reverse = 0, rem;
+            Console.Write("Enter a number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.Write("Reversed Number: " + reverse);
+        }
+    }
+}
