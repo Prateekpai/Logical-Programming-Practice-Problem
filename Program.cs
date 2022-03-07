16c60be27d786626389ae3046caed50de0fdec55
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-07 07:44:06 +0530

    Welcome To Logical Programming Practice Problem

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LogicalProgramming
+
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Welcome to Logical Programming Practice Probmel");
+        }
+    }
+}
