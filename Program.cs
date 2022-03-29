3c9f3a9b28d69a38cb4ff20cadc4cf888ac8ce72
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-29 07:14:56 +0530

    Stop Watch Problem

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+namespace StopWatchDemo
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            stopwatch.Stop();
+
+            Console.WriteLine("Time Elapaed {0}",stopwatch.Elapsed);
+        }
+    }
+}
