aec45b7563f0c0dc5919f938037033574742e7f0
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-29 07:13:15 +0530

    Coupon Number Problem

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace CouponNumber
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Distinct Number N:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            var stringChars = new char[6];
+            var random = new Random();
+
+            int j = 0;
+            while (j < n)
+            {
+                for (int i = 0; i < stringChars.Length; i++)
+                {
+                    stringChars[i] = chars[random.Next(chars.Length)];
+                }
+                Console.WriteLine(stringChars);
+                j++;
+            }
+            
+
+            //var finalString = new String(stringChars);
+        }
+
+    }
+}
